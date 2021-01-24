3a4dd6617688cd565f15ab523b5e5e985413ce3a
newbiegao <gaolk@gaolktekiMacBook-Air.local>
2021-01-24 22:41:13 +0800

    add test

--- src/test/java/org/keron/microservicevisualization/FlowchartsBuilderTest.java
+++ src/test/java/org/keron/microservicevisualization/FlowchartsBuilderTest.java
@@ -1,10 +1,7 @@
 package org.keron.microservicevisualization;
 
 import org.junit.jupiter.api.Test;
-import org.keron.microservicevisualization.model.flowcharts.AppNode;
-import org.keron.microservicevisualization.model.flowcharts.DataBaseNode;
-import org.keron.microservicevisualization.model.flowcharts.Graph;
-import org.keron.microservicevisualization.model.flowcharts.Link;
+import org.keron.microservicevisualization.model.flowcharts.*;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -90,6 +87,45 @@ public class FlowchartsBuilderTest {
         String text = builder.build(graph) ;
 
         assertThat(text).contains("db1") ;
+
+    }
+
+    @Test
+    public void treeNodeTest(){
+
+        FlowchartsBuilder builder = new FlowchartsBuilder() ;
+        Graph graph = new Graph() ;
+        graph.setLayout(Graph.TB);
+
+        // nodes
+        Node appNode = new AppNode("app1") ;
+        appNode.setTitle("app1");
+        graph.addNode(appNode) ;
+
+        Node appNode1 = new AppNode("app2") ;
+        appNode1.setTitle("app2");
+        graph.addNode(appNode1) ;
+
+        Node dataBaseNode = new DataBaseNode("db1") ;
+        dataBaseNode.setTitle("DataBase 1");
+        graph.addNode(dataBaseNode);
+
+        Node dataBaseNode1 = new DataBaseNode("db2") ;
+        dataBaseNode1.setTitle("DataBase 2");
+        graph.addNode(dataBaseNode1);
+
+        // links
+        Link link1 = new Link("app1" ,"app2") ;
+        Link link2 = new Link("app1" ,"db1") ;
+        Link link3 = new Link("app2" ,"db2") ;
+
+        graph.addLink(link1);
+        graph.addLink(link2);
+        graph.addLink(link3);
+
+        String text = builder.build(graph) ;
+
+        assertThat(text).contains("db2") ;
 
     }
 
