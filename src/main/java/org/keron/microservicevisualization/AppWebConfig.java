bd38810bfbdf837a93ee004694b710e69d5b66b8
gaolk <newbiegao@hotmail.com>
2021-02-18 00:45:33 +0800

    add app index page config

--- src/main/java/org/keron/microservicevisualization/AppWebConfig.java
+++ src/main/java/org/keron/microservicevisualization/AppWebConfig.java
@@ -1,5 +1,7 @@
 package org.keron.microservicevisualization;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +9,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class AppWebConfig implements WebMvcConfigurer {
 
+    @Value("${micro.vis.index}")
+    private String index ;
+
     @Override
     public void addViewControllers(ViewControllerRegistry registry){
 
-        registry.addViewController("/").setViewName("forward:app-index.html");
+        registry.addViewController("/").setViewName(index);
 
     }
 }
