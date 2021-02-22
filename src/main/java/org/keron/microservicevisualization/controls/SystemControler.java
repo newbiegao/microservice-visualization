bd4a5abfab91e8417eefb2fcfc96b270060bc9cc
gaolk <newbiegao@hotmail.com>
2021-02-22 17:30:59 +0800

    add system mamager

--- src/main/java/org/keron/microservicevisualization/controls/SystemControler.java
+++ src/main/java/org/keron/microservicevisualization/controls/SystemControler.java
@@ -6,8 +6,10 @@ import org.keron.microservicevisualization.service.DepartmentService;
 import org.keron.microservicevisualization.service.GraphService;
 import org.keron.microservicevisualization.service.SystemService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -21,7 +23,27 @@ public class SystemControler {
     @GetMapping("/chart/system/{sysId}")
     public SystemEntity loadSystemInfo(@PathVariable String sysId  ){
         Integer id = Integer.valueOf( sysId.replace("SYS","") );
-       return systemService.loadSystemEntity(id) ;
+        return systemService.loadSystemEntity(id) ;
+    }
+
+    @GetMapping("/data/systems/{proId}")
+    public List<SystemEntity> loadSystemListByProId( @PathVariable Integer proId ){
+        return systemService.loadSystemListByProId(proId) ;
+    }
+
+    @GetMapping("/data/system/{sysId}")
+    public SystemEntity loadSystemInfoById(@PathVariable Integer sysId ){
+        return systemService.loadSystemEntity(sysId) ;
+    }
+
+    @PostMapping("/data/system/delete")
+    public Boolean deleteSystem( Integer sysId ){
+        return systemService.deleteSystem(sysId) ;
+    }
+
+    @PostMapping("/data/system/save")
+    public SystemEntity saveSystem( Integer proId , Integer sysId , String sysName , String sysTitle , String sysType , Integer sysCount  ){
+        return systemService.saveSystemEntity( proId , sysId , sysName , sysTitle , sysType , sysCount ) ;
     }
 
 }
--- src/main/java/org/keron/microservicevisualization/service/SystemService.java
+++ src/main/java/org/keron/microservicevisualization/service/SystemService.java
@@ -5,6 +5,8 @@ import org.keron.microservicevisualization.repository.SystemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class SystemService {
 
@@ -15,4 +17,39 @@ public class SystemService {
         return systemRepository.loadSystemEntity(sysId) ;
     }
 
+    public List<SystemEntity> loadSystemListByProId (Integer proId ){
+        return systemRepository.loadSystemEntityListByProId(proId) ;
+    }
+
+    /**
+     *
+     * @param proId
+     * @param sysId
+     * @param sysName
+     * @param sysTitle
+     * @param sysType
+     * @param sysCount
+     * @return
+     */
+    public SystemEntity saveSystemEntity( Integer proId , Integer sysId , String sysName , String sysTitle , String sysType , Integer sysCount ){
+        SystemEntity systemEntity = systemRepository.loadSystemEntity(sysId) ;
+        if( systemEntity == null ){
+            systemEntity = new SystemEntity() ;
+            systemEntity.setId(sysId);
+        }
+        systemEntity.setName(sysName);
+        systemEntity.setProId(proId);
+        systemEntity.setTitle(sysTitle);
+        systemEntity.setType(sysType);
+        systemEntity.setCount(sysCount);
+        systemRepository.saveAndFlush(systemEntity) ;
+        return systemEntity ;
+    }
+
+    public boolean deleteSystem( Integer sysId ){
+        // TODO: delete node link entity
+
+        systemRepository.deleteById(sysId);
+        return true ;
+    }
 }
